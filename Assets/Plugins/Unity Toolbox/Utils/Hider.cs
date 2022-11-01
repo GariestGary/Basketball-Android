426fa01edd94d9fb61359e660bfd9d9a95a5e555
GariestGary <rtutede@gmail.com>
2022-11-01 15:22:43 +0300

    initial commit

---
+++ Assets/Plugins/Unity Toolbox/Utils/Hider.cs
@@ -0,0 +1,32 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+namespace VolumeBox.Toolbox
+{
+    public class Hider : EditorWindow 
+    {
+        [MenuItem("Tools/Hider")]
+        private static void ShowWindow() 
+        {
+            var window = GetWindow<Hider>();
+            window.titleContent = new GUIContent("Hider");
+            window.Show();
+        }
+    
+        private void OnGUI() 
+        {
+            if(GUILayout.Button("Unhide"))
+            {
+                var hidden = GameObject.FindObjectsOfType<GameObject>();
+
+                foreach(var h in hidden)
+                {
+                    h.hideFlags = HideFlags.None;
+                }
+            }
+        }
+    }
+}
+#endif
